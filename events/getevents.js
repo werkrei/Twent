a2189639e09b895b8b5fe000612d89a960800843
werkrei <dreamkeeper34@gmail.com>
2024-02-29 00:34:18 +0300

    We were looking for the description wrong, fixed that, added some edge case scenario bullshit.

--- events/getevents.js
+++ events/getevents.js
@@ -16,15 +16,18 @@ module.exports = {
 	name: Events.GuildScheduledEventUpdate,
 	async execute(createdevent) {
 		// Put the description in a variable, if the description is too long, it will get truncated at line 34
-		let truncDesc = createdevent.description;
+		let truncDesc = '';
+		console.log(createdevent.description);
 		let tweet = `MondoCat Event starting: ${createdevent.name}\n ${truncDesc}\nYou can join this event on our Discord server https://discord.gg/mondocat`;
 		// Configure the tweet in a const so we can calculate the characters and not have to change the tweet
 		// Use tweetNoVariables to calculate character limit, tweet to send.
 		const tweetNoVariables = 'MondoCat Event starting: You can join this event on our Discord server https://discord.gg/mondocat';
 		// Find the point in the description where we need to cut from
-		const descriptionIndex = createdevent.description.search(':`');
+		const descriptionIndex = createdevent.description.search('\u{1D05}\u{1D07}\u{A731}\u{1D04}\u{280}\u{26A}\u{1D18}\u{1D1B}\u{26A}\u{1D0F}\u{274}');
+		console.log(descriptionIndex);
 		// Cut the description
-		const description = createdevent.description.substring(descriptionIndex + 1, descriptionIndex.length);
+		const description = createdevent.description.substring(descriptionIndex + 13, descriptionIndex.length);
+		console.log(description);
 		// Set the offset for the Twitter character limit, the tweet we send has 98 static characters
 		const characterLimitOffset = tweetNoVariables.length + createdevent.name.length;
 		// Add the offset to the limit
@@ -34,6 +37,9 @@ module.exports = {
 			truncDesc = description.substring(1, characterLimit - 20) + '...';
 			// We need to set the variables in the string, and this is the only way I could come up with💀
 			tweet = `MondoCat Event starting: ${createdevent.name}\n ${truncDesc}\nYou can join this event on our Discord server https://discord.gg/mondocat`;
+		}
+		else {
+			tweet = `MondoCat Event starting: ${createdevent.name}\n ${description}\nYou can join this event on our Discord server https://discord.gg/mondocat`;
 		}
 		if (createdevent.status == '1') {
 			console.log('Event Starting:');
